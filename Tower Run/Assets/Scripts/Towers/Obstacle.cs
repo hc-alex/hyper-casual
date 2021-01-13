0f36b78845e71e0324453cab4b7a2e8a1f472772
akos <to.easyweb@gmail.com>
2021-01-13 17:22:41 +0200

    Fixed obstacle logic

--- Tower Run/Assets/Scripts/Towers/Obstacle.cs
+++ Tower Run/Assets/Scripts/Towers/Obstacle.cs
@@ -1,16 +1,52 @@
 ﻿using Player;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Towers
 {
   public class Obstacle : MonoBehaviour
   {
-    private void OnTriggerEnter(Collider other)
+    [SerializeField] private float _humanHeight;
+    private int _humanCount;
+
+    private bool _isCollisionEntered;
+
+    private void Start()
     {
-      if (!other.TryGetComponent(out PlayerTower playerTower))
+      _humanCount = SetHumanCount;
+      Vector3 newScale = SetScale();
+      SetPosition(newScale);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+      if (_isCollisionEntered)
+        return;
+      
+      if (!other.gameObject.TryGetComponent(out PlayerTower playerTower))
         return;
 
-      playerTower.Explode(Random.Range(1, 3));
+      _isCollisionEntered = true;
+      
+      playerTower.Explode(_humanCount);
+    }
+
+    private int SetHumanCount =>
+      Random.Range(1, 3);
+
+    private void SetPosition(Vector3 newlocalScale)
+    {
+      Vector3 newPosition = transform.position;
+      newPosition.y += newlocalScale.x / 2;
+      transform.position = newPosition;
+    }
+
+    private Vector3 SetScale()
+    {
+      Vector3 newScale = transform.localScale;
+      newScale.x = _humanHeight * _humanCount;
+      transform.localScale = newScale;
+      return newScale;
     }
   }
 }
--- Tower Run/Assets/Scripts/Towers/Spawners/ObstacleSpawner.cs
+++ Tower Run/Assets/Scripts/Towers/Spawners/ObstacleSpawner.cs
@@ -2,6 +2,14 @@
 {
   public class ObstacleSpawner : Spawner<Obstacle>
   {
+    protected override void OnTowerReached(int towerNumber)
+    {
+      if (towerNumber == 0)
+        return;
+      
+      Spawn(CalculateDistance(towerNumber));
+    }
+  
     protected override float CalculateDistance(int towerNumber) => 
       (towerNumber + 1 - 0.5f) * _distanceBetweenTower;
   }
